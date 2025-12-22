9a02a9124c367bf0527f49f249e119fc747d82e9
krllkrbv <krllkrbv@mail.ru>
2025-12-22 20:04:42 +0300

    update src

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,12 +1,5 @@
-import i18next from 'i18next';
-import ru from './ru.js';
+import ru from './ru/translation.json'
 
-const initI18n = () => i18next.init({
-  lng: 'ru',
-  debug: false,
-  resources: {
-    ru,
-  },
-});
-
-export default initI18n;
+export default {
+  ru: { translation: ru },
+}

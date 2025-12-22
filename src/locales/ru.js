bc31035df7c71b09db7f19eb502b2ef537b8b3fd
krllkrbv <krllkrbv@mail.ru>
2025-12-22 20:23:52 +0300

    update src

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,11 +1,11 @@
-{
-  "rssLoaded"; "RSS успешно загружен",
-  "rssExists"; "RSS уже существует",
-  "empty"; "Не должно быть пустым",
-  "invalidUrl"; "Ссылка должна быть валидным URL",
-  "noValidRss"; "Ресурс не содержит валидный RSS",
-  "invalidRss"; "Ресурс не содержит валидный RSS",
-  "unknown"; "Неизвестная ошибка",
-  "view"; "Просмотр",
-  "networkError"; "Ошибка сети"
-}
+export default {
+  "rssLoaded": "RSS успешно загружен",
+  "rssExists": "RSS уже существует",
+  "empty": "Не должно быть пустым",
+  "invalidUrl": "Ссылка должна быть валидным URL",
+  "noValidRss": "Ресурс не содержит валидный RSS",
+  "invalidRss": "Ресурс не содержит валидный RSS",
+  "unknown": "Неизвестная ошибка",
+  "view": "Просмотр",
+  "networkError": "Ошибка сети"
+};

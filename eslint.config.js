b24a0d12434ab6b88869771a3fa700ea414f788c
krllkrbv <krllkrbv@mail.ru>
2025-12-28 18:07:27 +0300

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,24 @@
-module.exports = {}
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.js'],
+    ignores: ['dist/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+  {
+    files: [
+      'eslint.config.js',
+      'webpack.config.js',
+      'postcss.config.js',
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];

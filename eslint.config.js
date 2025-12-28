ae3cfe2aab9e8902c0af028ef7bfe2de451f76c1
krllkrbv <krllkrbv@mail.ru>
2025-12-28 18:07:29 +0300

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,65 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import globals from 'globals';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
-  js.configs.recommended,
-
   {
-    files: ['src/**/*.js'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
+    ignores: ['dist/'],
   },
-
   {
-    files: [
-      '**/webpack.config.js',
-      '**/postcss.config.js',
-    ],
     languageOptions: {
-      sourceType: 'commonjs',
       globals: {
         ...globals.node,
+        ...globals.jest,
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
     },
+    plugins: { import: importPlugin },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
+    },
   },
-
+  ...compat.extends('airbnb-base'),
   {
-    ignores: ['dist/**'],
+    rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/extensions': ['error', 'ignorePackages'],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      semi: 'off',
+      'linebreak-style': 'off',
+      'import/no-unresolved': 'off',
+      'import/order': 'off',
+      'arrow-parens': 'off',
+      'no-use-before-define': 'off',
+      'brace-style': 'off',
+      'max-len': 'off',
+      'arrow-body-style': 'off',
+      'no-new': 'off',
+      'object-curly-newline': 'off',
+      'no-param-reassign': 'off',
+    },
   },
-]
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-/* eslint-env node */
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
